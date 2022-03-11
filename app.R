5501469d8e95d1bb6798ca286c8271cb36f9401e
YangWu1227 <yangwu2020@gmail.com>
2022-03-11 01:45:48 -0500

    fix: add namespace to disambiguate runApp

--- app.R
+++ app.R
@@ -26,5 +26,5 @@ top5 <- cranlogs::cran_top_downloads(count = 5)$package
 today <- Sys.Date() - 1
 last_month <- today - 60
 
-runApp(top5, today, last_month, pool)
+TrackerDashboard::runApp(top5, today, last_month, pool)
 
