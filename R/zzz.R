02da04da371c76ceb3217f4dd5f54e1ac55b3510
YangWu1227 <yangwu2020@gmail.com>
2022-07-08 02:21:12 -0400

    docs:add documentation for .onAttach

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,12 @@
+#' Add directory of static resources to Shiny's web server
+#'
+# The function `addResourcePath()` makes static files in 'www' available to app components.
+#'
+#' @param libname A character vector giving the library directory where the package defining the namespace was found.
+#' @param pkgname A character vector giving the name of the package.
+#'
 #' @importFrom shiny addResourcePath
-# The function addResourcePath() makes static files in 'www' available to app components (secure ui)
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage("Welcome to my dashboard!")
-  addResourcePath('www', system.file("www", package = "TrackerDashboard"))
+  addResourcePath("www", system.file("www", package = "TrackerDashboard"))
 }
